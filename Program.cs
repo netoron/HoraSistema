01e2faba1ef5cf21d3aa4d216e1c771035e88e34
Erick. santos <erickblz@outlook.com>
2020-06-05 22:15:51 -0300

    Adicionando Programa de data e hora

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime HoraSistema = DateTime.Now;
+            Console.WriteLine("Exibe Data e Hora");
+            Console.WriteLine($"Agora é {HoraSistema}");
+            
+        }
+    }
+}
